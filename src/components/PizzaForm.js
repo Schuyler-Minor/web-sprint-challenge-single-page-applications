29f00639cacc63421fd4727c390b6d05e1c51175
Schuyler <skym1n0r91@gmail.com>
2022-01-28 14:06:26 -0600

    checkboxes started

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -3,7 +3,12 @@ import React from "react";
 
 
 
-function PizzaForm () {
+function PizzaForm (props) {
+    const { values, update, submit} = props;
+
+    const onChange = evt => {
+        const { value } = evt.target;
+    }
     return (
         <div>
             <h1>Do You Like Pizza?</h1>
@@ -12,13 +17,44 @@ function PizzaForm () {
                 <input id="name-input"
                 value=""
                 type="text"
+                onChange={onChange}
                 />
                 </label>
                <label>
-                   <select id="size-dropdown">
-
+                   <select id="size-dropdown" onChange={onChange}>
+                       <option>--Select a Size--</option>
+                       <option>Small</option>
+                       <option>Medium</option>
+                       <option>Large</option>
                    </select>
                </label>
+
+               <div className="toppings">Toppings
+               <label>Pepporoni
+                   <input 
+                    type="checkbox"
+                   />
+               </label>
+               <label>Sausage
+                <input 
+                type="checkbox"
+                />
+               </label>
+               <label>Black Olives
+                   <input 
+                   type="checkbox"
+                   />
+
+               </label>
+
+               <label>Jalepenos
+                   <input 
+                   type="checkbox"
+                   />
+
+               </label>
+               </div>
+              
 
             </form>
         </div>
