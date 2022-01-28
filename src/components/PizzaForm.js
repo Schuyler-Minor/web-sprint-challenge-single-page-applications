f195a2e1b910b65dc0fe80a9cc1bbabca8498ba9
Schuyler <skym1n0r91@gmail.com>
2022-01-28 13:40:04 -0600

    pizza size id is correct

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -8,10 +8,17 @@ function PizzaForm () {
         <div>
             <h1>Do You Like Pizza?</h1>
             <form id="pizza-form">
+                <label>Name
                 <input id="name-input"
                 value=""
                 type="text"
                 />
+                </label>
+               <label>
+                   <select id="size-dropdown">
+
+                   </select>
+               </label>
 
             </form>
         </div>
