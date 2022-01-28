b49d0989d7827168d1fb9dec39afa038877e653b
Schuyler <skym1n0r91@gmail.com>
2022-01-28 12:47:08 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import PizzaForm from "./components/PizzaForm";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+   <div>
+    
+    <nav>
+    <h1>BloomTech Eats</h1>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/pizza'>Pizza</Link>
+      </div>
+    </nav>
+    <Switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route>
+        <PizzaForm />
+      </Route>
+    </Switch>
+    
+   </div>
   );
 };
 export default App;
---
+++ src/components/PizzaForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PizzaForm () {
+    return (
+        <div>
+            <h1>Do You Like Pizza?</h1>
+        </div>
+    )
+}
+
+export default PizzaForm;
